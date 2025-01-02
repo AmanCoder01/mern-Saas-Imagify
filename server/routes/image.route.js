198def9903022cfd1c9344ec2401a13647b401cc
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2025-01-02 22:46:06 +0530

    error

--- server/routes/image.route.js
+++ server/routes/image.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import verifyUserToken from "../middlewares/verifyToken.js";
-import { generateImage, getAllImages } from "../controllers/image.controller.js";
+import { deleteImage, generateImage, getAllImages } from "../controllers/image.controller.js";
 
 
 const router = express.Router();
 
 router.post("/generate-image", verifyUserToken, generateImage);
 router.get("/all", verifyUserToken, getAllImages);
+router.delete("/delete/:imageId", verifyUserToken, deleteImage);
 
 export default router
